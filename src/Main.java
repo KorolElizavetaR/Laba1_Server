384e8480b4618037b308dc976202772f287216c8
KorolER <elizavetakorol2005@gmail.com>
2024-02-12 21:07:24 +0300

    ASDASD

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+    private static final int PORT = 1234;
+
+    public static void main(String[] args) {
+        try {
+            Server();
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage() + " : " + ex.getCause());
+        }
+    }
+    public static int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+
+    public static void Server() throws Exception
+    {
+        ServerSocket server = new ServerSocket(PORT);
+        Socket socket = server.accept();
+
+        Scanner in = new Scanner(socket.getInputStream());
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        int A = in.nextInt();
+        //int B = in.nextInt();
+
+        out.println(A);
+        socket.close();
+    }
+}
